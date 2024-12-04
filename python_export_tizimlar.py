f004b671b849d0a71a033de5125c7e69122e5b64
Adolatxon <adolathonibrohimova4@gmail.com>
2024-12-04 09:55:12 +0500

    Add files via upload

---
+++ python_export_tizimlar.py
@@ -0,0 +1,14 @@
+import csv
+ 
+# 2D o'zgaruvchilar ro'yxati (satr va ustunlar bilan jadval ma'lumotlari)
+input_variable = [
+    ['S.no','name','e-mail'],
+    [1,'meesha','meesh@email.com'],
+    (2,'abhilasha','ab@email.com'),
+    (3,'arav','arav123@email.com')
+]
+ 
+# Example.csv joriy ishchi katalogda yaratiladi
+with open ('Example.csv','w',newline = '') as csvfile:
+    my_writer = csv.writer(csvfile, delimiter = ' ')
+    my_writer.writerows(input_variable)
